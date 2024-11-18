24f488c03cb3408a5de6aeadc0bbd9dcce7e348d
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-18 14:40:46 +0500

    create: find last letter func

---
+++ functionsBasics/lastLetter.js
@@ -0,0 +1,9 @@
+// returns the last letter of given string 
+
+let userInput = prompt("Please enter a string:");
+
+function lastLetter(userInput) {
+    return userInput[-1];
+}
+
+lastLetter(userInput);
