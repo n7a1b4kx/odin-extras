8ebeb6e0100450bcd46885daf8eb1c82815c626e
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-18 13:19:21 +0500

    fix: correct file name remove the num

--- functionsBasics/add7.js
+++
@@ -1,9 +0,0 @@
-// this functions takes an input of int or any given number and returns a value by adding 7 number into it i.e num given is 3 add 7 return 10
-
-let userInput = prompt("Type a number:");
-
-function add(userInput) {
-    return +userInput + 7;
-}
-
-add(userInput);
