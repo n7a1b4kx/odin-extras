6004b02bdd4e081009386de009c00c36fe084993
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-19 14:31:15 +0500

    create: boile plate of the func fizzBuzz
    created and added the first condition

---
+++ fizzBuzz/fizzBuzz.js
@@ -0,0 +1,11 @@
+// creating very first program which is asked in interviews 
+
+// gettin' input 
+let userInput = "6"//prompt("Please enter a number: ");
+
+for (let i = 0; i < userInput; i++) {
+    if (userInput % 3 === 0) {
+        console.log("FIzz");
+    }
+}
+
