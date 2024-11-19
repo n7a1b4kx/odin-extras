9ca906d52fe8480fa0b89b96d9348879664a72f8
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-19 14:39:54 +0500

    fix: convert str to int and correct the for loop start

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,11 +1,19 @@
 // creating very first program which is asked in interviews 
 
 // gettin' input 
-let userInput = "6"//prompt("Please enter a number: ");
+let userInput = parseInt(prompt("Please enter a number: "));
 
-for (let i = 0; i < userInput; i++) {
-    if (userInput % 3 === 0) {
+for (let i = 1; i <= userInput; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FIzzBuzz");
+    }
+    else if (i % 3 === 0) {
         console.log("FIzz");
     }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
 }
-
