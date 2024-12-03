6e47b6a098ab70d752f734ae05274f926968da78
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-03 15:48:48 +0500

    add: h3 tag

--- domManipulation/script.js
+++ domManipulation/script.js
@@ -12,6 +12,14 @@ container.appendChild(content);
 const para = document.createElement("p");
 para.classList.add("paragraph");
 para.textContent = "Hey I'm red!";
-para.setAttribute("style", "color: red; font-size: 24px; text-align: center;");
+para.setAttribute("style", "color: red; text-align: center;");
 
 container.appendChild(para);
+
+const heading3 = document.createElement("h3");
+heading3.classList.add("heading");
+heading3.textContent = "I'm blue h3";
+heading3.setAttribute("style", "color: blue; text-align: center;");
+
+container.appendChild(heading3);
+
