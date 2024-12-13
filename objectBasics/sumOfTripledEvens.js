eb5adc18e26f0a45d58d43641ff038c918ea08f3
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-13 15:07:32 +0500

    Add: func for checking Even numbers

---
+++ objectBasics/sumOfTripledEvens.js
@@ -0,0 +1,27 @@
+// using arrays methods to recreate this func
+
+```
+function sumOfTripledEvens(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      // Step 1: If the element is an even number
+      if (array[i] % 2 === 0) {
+        // Step 2: Multiply this number by three
+        const tripleEvenNumber = array[i] * 3;
+  
+        // Step 3: Add the new number to the total
+        sum += tripleEvenNumber;
+      }
+    }
+    return sum;
+}
+```
+// here is my implementation
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+function sumOfTripledEvens(array) {
+    
+}
