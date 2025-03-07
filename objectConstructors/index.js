cf6a964d9ab004ab4968379b98644afe4b01d07c
n7a1b4kx <169384643+n7a1b4kx@users.noreply.github.com>
2025-03-07 19:43:22 +0500

    create: the hobbit book object
    fixed formated string in .info attribute

--- objectConstructors/index.js
+++ objectConstructors/index.js
@@ -1,11 +1,16 @@
 // this is test exercise to create objects in javascript using object constructor
 
+// object constructor
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        console.log("The ${title} by ${author}, ${pages} pages, read ${read}.")
+        console.log(`${title} by ${author}, ${pages} pages, ${read}.`)
     };
 }
+
+// creating a new object TheHobbit book
+const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295, "Not read yet");
+theHobbit.info();
