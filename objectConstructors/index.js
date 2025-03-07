3099ee27a473c6e0a25e91fe72bae7739f29ce76
n7a1b4kx <169384643+n7a1b4kx@users.noreply.github.com>
2025-03-07 19:35:17 +0500

    create: book object constructor

--- objectConstructors/index.js
+++ objectConstructors/index.js
@@ -1,1 +1,11 @@
 // this is test exercise to create objects in javascript using object constructor
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        console.log("The ${title} by ${author}, ${pages} pages, read ${read}.")
+    };
+}
